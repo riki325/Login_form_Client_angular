4c923032a5a2ba7ef7cd43ab1aaa51340a0eddd0
riki325 <r0504153337@gmail.com>
2023-02-14 17:26:56 +0200

    change

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,7 +3,6 @@ import PrivateUserDetails from '../Models/PrivateUserDetails';
 import { UserService } from '../user.service';
 
 import {FormControl, Validators} from '@angular/forms';
-import { JsonpInterceptor } from '@angular/common/http';
 
 
 @Component({
@@ -55,31 +54,9 @@ this.userService.currentUserService.FamilyId=this.familyId++;
         console.log(err)
     });
     
-    // this.userService.addUser(this.userService.currentUserService).subscribe((succ) => {
-    //          console.log(succ)});
-
+    
     console.log(this.userService.currentUserService)
-    // this.userService.currentUserService.Genus == "male" ? this.userService.currentUserService.Status == "father" : this.userService.currentUserService.Status == "mother";
-    // this.userArr.push(this.userService.currentUserService);
-    // if (this.childArr.length > 0) {
-    //   // var res = this.all.find(u => u.IdNumber == this.childArr[0].IdNumber);
-    //   // if (res != undefined){
-    //   //   this.userService.currentUserService.FamilyId = res.FamilyId;
-    //     this. childArr.forEach(c => { 
-    //       this.child.HMO = this.user.HMO;
-    //       this.userArr.push(c);
-    //      ; })
-    //     }
-    //     this.userArr.forEach(u => { 
-    //       this.userService.addUser(u).subscribe((succ) => {
-    //         console.log(succ)})
-    //     });
   }
-  // save() {
-  //   this.user = this.userService.currentUserService;
-  //   this.userService.saveUser(this.user).subscribe((response: any) => {
-  //     console.log(response);
-  //   });}
   
   getErrorMessage() {
     if (this.childFName.hasError('required')) {
@@ -94,16 +71,13 @@ moreChild() {
   this.display = true;
 }
 
-
 addChild() {
   this.child.Status="child";
   this.child.HMO=this.userService.currentUserService.HMO;
   this.child.FamilyId=this.userService.currentUserService.FamilyId;
   console.log(this.child)
   this.userService.addUser(this.child).subscribe((succ) => {
-   
     console.log("succ")})
-    // this.childArr.push(this.child);
     this.child=new PrivateUserDetails(0, " ", " ", " ", null, " ", " ", -1, " ");
 this.display = false;
 }
@@ -125,6 +99,5 @@ anchor.setAttribute("href",c);
 anchor.setAttribute("downLoad","somedata.csv");
 document.body.append(anchor);
 anchor.click();
-
-}
+}gi
 }
